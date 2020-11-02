bb0b2d337389da15e97da10e711b9cef0120bd87
shahrukh-compucorp <shahrukh@compucorp.co.uk>
2020-11-02 11:59:32 +0500

    CATL-1900: Change code formatting

--- js/create-pdf-form.js
+++ js/create-pdf-form.js
@@ -1,9 +1,10 @@
 (function ($) {
-
   $(document).on('crmLoad', function () {
-
-    function redirectOnFileDownload() {
-      if(isFormValid()) {
+    /**
+     * Redirect the user after pdf generation.
+     */
+    function redirectOnFileDownload () {
+      if (isFormValid()) {
         var caseId = getCaseId();
         if (caseId) {
           var urlWithTab = updateQueryStringParameter('tab', 'Activities');
@@ -17,7 +18,12 @@
       }
     }
 
-    function getCaseId() {
+    /**
+     * Returns the case id if available otherwise 0.
+     *
+     * @returns {number} case id.
+     */
+    function getCaseId () {
       var entryUrlElem = $('[name="entryURL"]');
       var caseId = 0;
       if (entryUrlElem.length) {
@@ -28,14 +34,26 @@
       return caseId;
     }
 
-    function isFormValid() {
-      return $('.CRM_Contact_Form_Task_PDF').find('.crm-error:not(.valid)').length == 0;
+    /**
+     * Checks whether the form has errors or not.
+     *
+     * @returns {boolean} form validity.
+     */
+    function isFormValid () {
+      return $('.CRM_Contact_Form_Task_PDF').find('.crm-error:not(.valid)').length === 0;
     }
 
-    function getParameterByName(name, url = window.location.href) {
-      name = name.replace(/[\[\]]/g, '\\$&');
-      var regex = new RegExp('[?&;]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
+    /**
+     * Returns parameter value from a query string.
+     *
+     * @param {string} name  name of the parameter.
+     * @param {string} url query string.
+     * @returns {any} parameter value.
+     */
+    function getParameterByName (name, url = window.location.href) {
+      name = name.replace(/[[]]/g, '\\$&');
+      var regex = new RegExp('[?&;]' + name + '(=([^&#]*)|&|#|$)');
+      var results = regex.exec(url);
       if (!results) {
         return null;
       }
@@ -45,14 +63,21 @@
       return decodeURIComponent(results[2].replace(/\+/g, ' '));
     }
 
-    function updateQueryStringParameter(key, value, uri = window.location.href) {
-      var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
-      var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+    /**
+     * Add or update a parameter in a url.
+     *
+     * @param {string} key parameter name.
+     * @param {any} value parameter value.
+     * @param {string} uri query string.
+     * @returns {string} updates query string.
+     */
+    function updateQueryStringParameter (key, value, uri = window.location.href) {
+      var re = new RegExp('([?&])' + key + '=.*?(&|$)', 'i');
+      var separator = uri.indexOf('?') !== -1 ? '&' : '?';
       if (uri.match(re)) {
-        return uri.replace(re, '$1' + key + "=" + value + '$2');
-      }
-      else {
-        return uri + separator + key + "=" + value;
+        return uri.replace(re, '$1' + key + '=' + value + '$2');
+      } else {
+        return uri + separator + key + '=' + value;
       }
     }
 
