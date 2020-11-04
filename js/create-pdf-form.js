60a317b6ec70705b6111ae5ec873948b57e53b50
Debarshi Bhaumik <debarshi@compucorp.co.uk>
2020-11-04 11:20:23 +0000

    CATL-1938: Validate filename before submit

--- js/create-pdf-form.js
+++ js/create-pdf-form.js
@@ -1,9 +1,9 @@
 (function ($, _) {
-  $(document).on('crmLoad', function () {
-    var $form, $dialog;
+  $(document).one('crmLoad', function () {
+    var $dialog;
 
     (function init () {
-      $form = $('.CRM_Contact_Form_Task_PDF');
+      var $form = $('.CRM_Contact_Form_Task_PDF');
 
       $('body').off('submit', $form);
       $('body').on('submit', $form, openFileNamePopup);
@@ -19,6 +19,7 @@
         return;
       }
 
+      var $form = $(e.target);
       var $clickedButtonIdentifier = $(document.activeElement).data('identifier');
       var isDownloadDocumentButtonClicked = $clickedButtonIdentifier === 'buttons[_qf_PDF_upload]';
 
@@ -31,8 +32,10 @@
       e.preventDefault();
 
       $dialog = $('<div class="civicase__pdf-filename-dialog"></div>')
-        .html(getPopupMarkUp())
+        .html(getPopupMarkUp($form))
         .dialog(getPopupSettings());
+
+      $('form', $dialog).crmValidate();
     }
 
     /**
@@ -47,12 +50,14 @@
     /**
      * Get html markup for the filename popup
      *
+     * @param {object} $form form element
+     *
      * @returns {string} markup
      */
-    function getPopupMarkUp () {
+    function getPopupMarkUp ($form) {
       var subject = $form.find('#subject').val();
 
-      return '<form>' +
+      return '<form id="civicase__pdf-filename-form">' +
         '<div class="crm-form-block crm-block crm-contact-task-pdf-form-block">' +
           '<table class="form-layout-compressed">' +
              '<tbody>' +
@@ -63,7 +68,7 @@
                 '</tr>' +
                 '<tr>' +
                   '<td>' +
-                    '<input type="text" id="pdf_filename" pattern="[a-zA-Z0-9-_. ]+" class="crm-form-text" value=' + subject + '>' +
+                    '<input type="text" id="pdf_filename" pattern="[a-zA-Z0-9-_. ]+" class="huge crm-form-text required" value="' + subject + '">' +
                   '</td>' +
                 '</tr>' +
               '</tbody>' +
@@ -80,14 +85,18 @@
     function getPopupSettings () {
       return {
         title: ts('Download Document'),
-        width: 500,
+        width: 'auto',
         height: 'auto',
         buttons: [
           {
             text: 'Download Document',
             click: function () {
+              if (!$('#civicase__pdf-filename-form').valid()) {
+                return;
+              }
               $('#pdf_filename_hidden').remove();
 
+              var $form = $('.CRM_Contact_Form_Task_PDF');
               $('<input>').attr({
                 type: 'hidden',
                 id: 'pdf_filename_hidden',
@@ -95,7 +104,7 @@
                 value: $('#pdf_filename').val()
               }).appendTo($form);
 
-              $dialog.dialog('destroy').remove();
+              $dialog.remove();
               $('[data-identifier="buttons[_qf_PDF_upload]"]').trigger('click');
               $('#pdf_filename_hidden').remove();
             }
@@ -103,7 +112,7 @@
           {
             text: 'Cancel',
             click: function () {
-              $dialog.dialog('destroy').remove();
+              $dialog.remove();
             }
           }
         ]
